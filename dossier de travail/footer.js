102d1be3166cae6046ee4710b81945c568ef20ab
kengnidoriane <dorianekengni@gmail.com>
2024-06-07 16:50:42 +0100

    structure de base avec flex

---
+++ dossier de travail/footer.js
@@ -0,0 +1,11 @@
+let link_team48 = document.querySelector(".link_team48");
+console.log(link_team48.children[1]);
+
+function show_team48_summary(param) {
+    if (param) {
+        link_team48.children[1].classList.add("active_hidden_item");
+    } else {
+        link_team48.children[1].classList.remove("active_hidden_item");
+    }
+    
+}
