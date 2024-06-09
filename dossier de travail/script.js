50cd773fb9dd2fa6cc54a6ec2224f5727074c44d
kengnidoriane <dorianekengni@gmail.com>
2024-06-09 16:34:47 +0100

    Ajout des produits de boulangerie

--- dossier de travail/script.js
+++ dossier de travail/script.js
@@ -9,7 +9,16 @@ const blg_paragraph = document.querySelector("#blg_paragraph");
 const cos_paragraph = document.querySelector("cos_paragraph");
 const navLink = document.querySelectorAll(".navigation_link");
 const body = document.querySelector("body");
-console.log(navLink)
+let localisation = 1 // repersente les differents endroit du site
+// 1 accueil
+// 2 cosmetique
+// 3 boulangerie
+// 4 contact
+// 5 footer
+
+
+
+
 
 // ajoute la classe link active pour que le 
 //lien acceuil soit souligne quand on est sur le containerWrapper
@@ -17,9 +26,12 @@ console.log(navLink)
 navLink[0].classList.add('link_active');
 
 function initialization() {
+    containerWrapper.style.transform = `translateX(0vw)`
     navLink.forEach(element => {
        element.classList.remove('link_active');
     });
+    body.style.overflowY = 'hidden'
+
 }
 
 
@@ -28,7 +40,9 @@ function initialization() {
 
 function acceuil_click() {
     initialization()
-    navLink[0].classList.add('link_active');
+    navLink[0].classList.add('link_active'); //pour souligner  le lien dans le navbar
+    localisation = 1;
+    
 }
 
 //fonction pour les modifications qui s'applique lorsqu'on
@@ -36,7 +50,10 @@ function acceuil_click() {
 
 function cosmetique_click() {
     initialization();
-    navLink[1].classList.add('link_active');
+    navLink[1].classList.add('link_active'); //pour souligner  le lien dans le navbar 
+    containerWrapper.style.transform = `translateX(50vw)`
+    body.style.overflowY = 'scroll'
+    localisation = 2;
     
 }
 
@@ -45,14 +62,44 @@ function cosmetique_click() {
 
 function boulangerie_click() {
     initialization();
-    navLink[2].classList.add('link_active');
-
+    navLink[2].classList.add('link_active'); //pour souligner  le lien dans le navbar 
+    containerWrapper.style.transform = `translate(-50vw)`
+    body.style.overflowY = 'scroll'
+    localisation = 3;
 }
+let random =  Math.floor(Math.random() * 2);
 
 
 function contact_click() {
     initialization();
-    navLink[3].classList.add('link_active');
+    navLink[3].classList.add('link_active'); //pour souligner  le lien dans le navbar 
+    switch (localisation) {
+        case 1:
+            
+            if(random === 0){
+                containerWrapper.style.transform = `translateX(50vw)`
+            }
+            else{
+                containerWrapper.style.transform = `translateX(-50vw)`
+            }
+
+            break;
+        case 2:
+            containerWrapper.style.transform = `translateX(50vw)`
+            
+            break;
+        case 3:
+            containerWrapper.style.transform = `translateX(-50vw)`
+            
+            break;
+        case 4:
+            
+            break;
+        default:
+            break;
+    }
+    body.style.overflowY = 'scroll'
+    localisation = 4;
 }
 
 
