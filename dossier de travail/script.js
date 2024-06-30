27f5d9e1eec24e1ed1bf7d5b40779f398a07161b
kengnidoriane <dorianekengni@gmail.com>
2024-06-30 14:44:23 +0100

    Ajout des premires elements du responsive

--- dossier de travail/script.js
+++ dossier de travail/script.js
@@ -1,14 +1,18 @@
 // declaration de variables
 
-
+const contact = document.querySelector('.contact');
 const containerWrapper = document.querySelector(".container_wrapper");
 const logo_gen = document.querySelector("#logo_gen");
 const logo_blg = document.querySelector("#logo_blg");
 const logo_cos = document.querySelector("#logo_cos");
-const blg_paragraph = document.querySelector("#blg_paragraph");
-const cos_paragraph = document.querySelector("cos_paragraph");
+const blgParagraph = document.querySelector("#blg_paragraph");
+const cosParagraph = document.querySelector("#cos_paragraph");
 const navLink = document.querySelectorAll(".navigation_link");
 const body = document.querySelector("body");
+const header = document.querySelector('.navigation');
+const headerHeight = header.offsetHeight;   // Obtenir la hauteur initiale du header
+
+
 let localisation = 1 // repersente les differents endroit du site
 // 1 accueil
 // 2 cosmetique
@@ -41,6 +45,8 @@ function initialization() {
 function acceuil_click() {
     initialization()
     navLink[0].classList.add('link_active'); //pour souligner  le lien dans le navbar
+    cosParagraph.style.display = 'block'
+    blgParagraph.style.display = 'block'
     localisation = 1;
     
 }
@@ -53,6 +59,7 @@ function cosmetique_click() {
     navLink[1].classList.add('link_active'); //pour souligner  le lien dans le navbar 
     containerWrapper.style.transform = `translateX(50vw)`
     body.style.overflowY = 'scroll'
+    cosParagraph.style.display = 'none'
     localisation = 2;
     
 }
@@ -65,14 +72,15 @@ function boulangerie_click() {
     navLink[2].classList.add('link_active'); //pour souligner  le lien dans le navbar 
     containerWrapper.style.transform = `translate(-50vw)`
     body.style.overflowY = 'scroll'
+    blgParagraph.style.display = 'none'
     localisation = 3;
 }
 let random =  Math.floor(Math.random() * 2);
-
+console.log(random);
 
 function contact_click() {
     initialization();
-    navLink[3].classList.add('link_active'); //pour souligner  le lien dans le navbar 
+    navLink[3].classList.add('link_active'); //pour souligner  le lien contact  dans le navbar 
     switch (localisation) {
         case 1:
             
@@ -102,33 +110,64 @@ function contact_click() {
     localisation = 4;
 }
 
+// fonction pour reduire la taille du header fixe au scroll
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 100) {
+        header.style.height = headerHeight - 35 + 'px';
+        
+        navLink.forEach(element => {
+            element.style.fontSize = '1rem';
+         });
+    }
+     else {
+
+        header.style.height = headerHeight + 'px';
+
+        navLink.forEach(element => {
+            element.style.fontSize = '1.3rem'
+         });
+    }
+});
 
 
-// function fosso_click(parametre) {
+function checkVisibility() {
 
-//     navLink.forEach(element => {
-//         element.classList.remove("link_active");
-//     });
-    
+    const rect = contact.getBoundingClientRect(); // Obtenir les dimensions et la position de la section contact
+    const top = rect.top; //extrait la position haut de la section.
+    const bottom = rect.bottom; //extrait la position bas de la section.
+    const viewportHeight = window.innerHeight;
+  
+    const visibleRatio = (bottom - top) / viewportHeight; // Calculer le ratio de visibilité
+  
+    if (top >= 0 && bottom <= window.innerHeight) {
+        navLink.forEach(element => {
+            element.classList.remove('link_active');
+         });
+      navLink[3].classList.add('link_active');
 
-//     switch (parametre) {
-//         case 'acceuil':
-//             acceuil_click();
-//             break;
-//         case 'cosmetique':
+      
+    } else {
+      if (random === 0) {
+        navLink.forEach(element => {
+            element.classList.remove('link_active');
+         });
+      navLink[1].classList.add('link_active');
+        
+      }
+      if(random === 1){
+        navLink.forEach(element => {
+            element.classList.remove('link_active');
+         });
+      navLink[2].classList.add('link_active');
 
-//             cosmetique_click();
-//             break;
-//         case 'boulangerie':
-//             boulangerie_click();
-//             break;
-//         case 'contact':
-//             contact_click();
-//             break;
-//         case 'apropos':
-//             apropos_click();
-//             break;
-//         default:
-//             break;
-//     }
-// }
+      }
+      navLink[3].classList.remove('link_active');
+
+    }
+  }
+  
+  window.addEventListener('scroll', checkVisibility);
+
+
